38ca8dcfad575e3c73bf3ba6d46cab22e93e749e
wangrenxiann <93121294+wangrenxiann@users.noreply.github.com>
2022-01-28 17:13:47 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,26 @@
-using JobBoard.Client.Services;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
-using Toolbelt.Blazor.Extensions.DependencyInjection;
 
-namespace JobBoard.Client
+namespace JobBoard.Server
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddHttpClient("CarRentalManagement.ServerAPI", (sp, client) => {
-                client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress); client.EnableIntercept(sp);
-            })
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-            // Supply HttpClient instances that include access tokens when making requests to the server project
-            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("JobBoard.ServerAPI"));
-
-            builder.Services.AddHttpClientInterceptor();
-            builder.Services.AddScoped<HttpInterceptorService>();
-            builder.Services.AddApiAuthorization();
-
-            await builder.Build().RunAsync();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
